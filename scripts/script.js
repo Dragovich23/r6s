23f296e32add09cc1bd4787db0c4ade65aabc8ba
Aleksander Borowczak <AleksBorow23@gmail.com>
2023-06-08 15:48:40 +0200

    ze last one

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-//
-
-
-
+//animacje
 function animacja() {
     $("img[class='img-fluid kartazdjecie']").animate({
         opacity: "1",
@@ -40,12 +37,48 @@ $(window).scroll(function() {
     }
  });
 
-    // $("img[class='box operator-icon']").click(function(){
-    //     if (licz < 1) {
-    //         setTimeout(animacja(), 700);
-    //         licz = 1;
-    //     }
-       
-    // });
+    
+
+    //dźwięki
+
+    $("li:contains('Vector .45 ACP'):eq(1)").click(function() {
+        $("#vector")[0].play();
+    });
+    $("li:contains('ITA12L'):eq(1)").click(function() {
+        $("#ita12")[0].play();
+    });
+    $("li:contains('MP7'):eq(1)").click(function() {
+        $("#mp7")[0].play();
+    });
+    $("li:contains('M870'):eq(1)").click(function() {
+        $("#m870")[0].play();
+    });
+    $("li:contains('G8A1'):eq(1)").click(function() {
+        $("#g8a1")[0].play();
+    });
+    $("li:contains('SuperNova'):eq(1)").click(function() {
+        $("#super")[0].play();
+    });
+    $("li:contains('M12'):eq(1)").click(function() {
+        $("#m12")[0].play();
+    });
+    $("li:contains('SPAS-15'):eq(1)").click(function() {
+        $("#spas")[0].play();
+    });
+    $("li:contains('DP27'):eq(1)").click(function() {
+        $("#dp27")[0].play();
+    });
+    $("li:contains('9×19VSN'):eq(1)").click(function() {
+        $("#vsn")[0].play();
+    });
+    $("li:contains('FMG-9'):eq(1)").click(function() {
+        $("#fmg")[0].play();
+    });
+    $("li:contains('SIX12 SD'):eq(1)").click(function() {
+        $("#six12")[0].play();
+    });
 
 });
+
+
+
